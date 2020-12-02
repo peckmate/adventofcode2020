5df21ff94bafd3d290ef66c9ae3457827f0d008b
Tyler Peck <tylerkmp@gmail.com>
2020-12-02 00:46:55 -0800

    part b again but it actually works this time

--- one/one_b.rb
+++ one/one_b.rb
@@ -2,23 +2,19 @@ require 'csv'
 
 =begin
 require 'csv' 
-@file = CSV.read('input.csv' ).flatten.map &:to_i 
+@file = CSV.read('set_of_numbers.csv' ).flatten.map &:to_i 
 =end 
 
-class OneB
+class OneB 
     YEAR = 2020
-
-    def self.compare( input )
-        set = input.each_cons( 2 ).map{ |x| p x }
-        input.each do |number| 
-            remainder = ( YEAR - number )
-            set.each do |smaller_set| 
-                if smaller_set.sum + number == 2020
-                    return [ smaller_set, number ].flatten
+    def self.compare( array_of_integers )
+        array_of_integers.each do |integer| 
+            remainder = YEAR - integer 
+            array_of_integers.combination(2).each do |x, y| 
+                if x + y == remainder
+                    return [x, y, integer ].sort
                 end
             end
         end
     end
-end 
-
-
+end
